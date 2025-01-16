fe0f99758e757dd9c901a5030afee1612d76f5c8
joshu <joshuaddington@gmail.com>
2025-01-16 16:04:32 -0700

    gpiozero testing

---
+++ Pin_factory.py
@@ -0,0 +1,2 @@
+from gpiozero import Device
+print(Device.pin_factory)
--- stepperScratch.py
+++ stepperScratch.py
@@ -1,1 +1,13 @@
-import pigpio as pig
+import gpiozero as gp
+from time import sleep
+
+
+
+# Set up the GPIO pin
+stepper = gp.LED(17)
+
+while True:
+    stepper.on()
+    sleep(0.01)
+    stepper.off()
+    sleep(0.01)

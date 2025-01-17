de61cd7490f122c1cd61a9d5ba2be73c899c3899
joshu <joshuaddington@gmail.com>
2025-01-16 17:12:06 -0700

    pi save

--- stepperScratch.py
+++ stepperScratch.py
@@ -2,12 +2,22 @@ import gpiozero as gp
 from time import sleep
 
 
+# Set up a list of devices to close at end
+devices = []
 
 # Set up the GPIO pin
 stepper = gp.LED(17)
+devices.append(stepper)
 
-while True:
-    stepper.on()
-    sleep(0.01)
-    stepper.off()
-    sleep(0.01)
+
+try:
+    while True:
+        stepper.on()
+        sleep(0.01)
+        stepper.off()
+        sleep(0.01)
+except KeyboardInterrupt:
+    print("Cleaning up GPIO")
+finally: 
+    for device in devices:
+        device.close()
